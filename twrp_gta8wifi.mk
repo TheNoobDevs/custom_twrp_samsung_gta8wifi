c0e53cee11efab6133b9c5b1c35bf7f170d05c02
TheNoobDevs <thenoobdevs2024@gmail.com>
2024-05-20 16:31:44 +0100

    cleanup

--- twrp_gta8wifi.mk
+++ twrp_gta8wifi.mk
@@ -9,9 +9,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
 # Release name
 PRODUCT_RELEASE_NAME := gta8wifi
 
